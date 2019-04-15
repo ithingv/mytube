be428d1ec951c0fd838f595ee91d66b7537c38bb
ithingv <ithingv@naver.com>
2019-04-15 16:08:03 +0900

    Enter

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const express = require('express');// node module을 가져오는것
+const app = express();
+const PORT = 4000;
+
+function handleListening(){
+    console.log(`Listening port : ${PORT}`);
+}
+app.listen(PORT, handleListening);
+/*
+const handleListening = () =>{   
+app.listen(PORT);
+}
+handleListening();
+*/
